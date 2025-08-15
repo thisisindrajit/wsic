ff5d41109b34217dc2e8c646b6393fd22cafad98
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-08-15 17:06:00 +0530

    small change

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { ArrowRight } from "lucide-react";
 const Home = () => {
   return (
     <>
-
       <TopBar />
       {/* Search box */}
       <div className="flex flex-col items-center justify-center flex-1 py-12 min-h-[calc(100vh-6rem)]">
@@ -17,13 +16,15 @@ const Home = () => {
             <Input
               type="text"
               placeholder="type in any topic"
-              className="h-auto border-x-0 border-t-0 border-foreground text-4xl/normal md:text-5xl/normal xl:text-6xl/normal font-light p-0 focus-visible:ring-none focus-visible:ring-[0px] focus-visible:border-cyan-500 focus-visible:text-cyan-500 transition-all"
+              autoFocus
+              className="h-auto border-x-0 border-t-0 text-4xl/normal md:text-5xl/normal xl:text-6xl/normal font-light p-0 focus-visible:ring-none focus-visible:ring-[0px] focus-visible:border-cyan-500 focus-visible:text-cyan-500 transition-all"
               maxLength={256}
             />
-            <div className="bg-cyan-500/10 flex items-center justify-center self-end md:self-auto h-16 md:h-auto mr-0 w-16 md:w-20 xl:w-24 text-cyan-500 hover:bg-cyan-500 hover:text-background transition-all cursor-pointer">
+            <div className="bg-cyan-500/5 flex items-center justify-center self-end md:self-auto h-14 md:h-auto mr-0 w-14 md:w-20 xl:w-24 text-cyan-500 hover:bg-cyan-500 hover:text-background transition-all cursor-pointer">
               <ArrowRight className="h-6 w-6 md:h-7 md:w-7 xl:h-8 xl:w-8" />
             </div>
           </div>
+          <div className="mt-4 xl:mt-8 text-lg font-light">Suggested</div>
         </div>
       </div>
       {/* Trending Blocks */}
