3da954cba3fa9fb00cf1c107e73470e0b4a35941
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-09-01 09:26:25 +0530

    added safe area for bottom bar and dynamic meta theme and bg color change using kiro vibe mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,5 +69,6 @@ export const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
-  userScalable: false
+  userScalable: false,
+  viewportFit: 'cover'
 };
--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,5 +1,5 @@
-export { default as Footer } from './Footer';
-export { default as TopBar } from './TopBar';
-export { default as Notification } from './Notification';
-export { default as ThemeToggle } from './ThemeToggle';
-export { default as MetaThemeAndBgColor } from './MetaThemeAndBgColor'
+export { default as Footer } from "./Footer";
+export { default as TopBar } from "./TopBar";
+export { default as Notification } from "./Notification";
+export { default as ThemeToggle } from "./ThemeToggle";
+export { default as MetaThemeAndBgColor } from "./MetaThemeAndBgColor";
